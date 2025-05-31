a3f4cfcab80e101de199b0180f318917e59606ca
deltaquerides <wajahat.syed@yahoo.fr>
2025-05-31 11:23:44 +0200

    change objects to objects of objects

--- utils/spriteConfigs.js
+++ utils/spriteConfigs.js
@@ -5,74 +5,81 @@ each property (like:walk_right, walk_left) contains metadata about the sprite
 */
 
 export const spriteConfigs = {
-  walk_right: {
-    src: "/images/dan/walk_right.png",
-    totalCols: 6,
-    totalRows: 1,
-    frameY: 0,
-  },
-  walk_left: {
-    src: "/images/dan/walk_left.png",
-    totalCols: 6,
-    totalRows: 1,
-    frameY: 0,
-  },
-  jump: {
-    src: "/images/dan/jump.png",
-    totalCols: 7,
-    totalRows: 1,
-    frameY: 0,
-  },
-  idle: {
-    src: "/images/dan/idle.png",
-    totalCols: 7,
-    totalRows: 1,
-    frameY: 0,
-  },
-  jump_left: {
-    src: "/images/dan/jump_left.png",
-    totalCols: 7,
-    totalRows: 1,
-    frameY: 0,
-  },
-  idle_left: {
-    src: "/images/dan/idle_left.png",
-    totalCols: 7,
-    totalRows: 1,
-    frameY: 0,
-  },
-  enemyExplode: {
-    src: "/images/enemy-death.png",
-    totalCols: 6,
-    totalRows: 1,
-    frameY: 0,
-    loopOnce: true,
-  },
-  playerDeathSprite: {
-    src: "/images/player-death.png",
-    totalCols: 6,
-    totalRows: 1,
-    frameY: 0,
-    frameSpeed: 8,
+  player: {
+    walk_right: {
+      src: "/images/dan/walk_right.png",
+      totalCols: 6,
+      totalRows: 1,
+      frameY: 0,
+    },
+    walk_left: {
+      src: "/images/dan/walk_left.png",
+      totalCols: 6,
+      totalRows: 1,
+      frameY: 0,
+    },
+    jump: {
+      src: "/images/dan/jump.png",
+      totalCols: 7,
+      totalRows: 1,
+      frameY: 0,
+    },
+    idle: {
+      src: "/images/dan/idle.png",
+      totalCols: 7,
+      totalRows: 1,
+      frameY: 0,
+    },
+    jump_left: {
+      src: "/images/dan/jump_left.png",
+      totalCols: 7,
+      totalRows: 1,
+      frameY: 0,
+    },
+    idle_left: {
+      src: "/images/dan/idle_left.png",
+      totalCols: 7,
+      totalRows: 1,
+      frameY: 0,
+    },
+    playerDeathSprite: {
+      src: "/images/player-death.png",
+      totalCols: 6,
+      totalRows: 1,
+      frameY: 0,
+      frameSpeed: 8,
 
-    loopOnce: true,
+      loopOnce: true,
+    },
   },
+  effects: {
+    enemyExplode: {
+      src: "/images/enemy-death.png",
+      totalCols: 6,
+      totalRows: 1,
+      frameY: 0,
+      loopOnce: true,
+    },
+  },
+
   enemyLuxmn: {
     src: "/images/enemyLuxmn.png",
     totalCols: 7,
     totalRows: 1,
     frameY: 0,
   },
-  enemyAmazonLeft: {
-    src: "/images/amazon/amazon_left.png",
-    totalCols: 8,
-    totalRows: 1,
-    frameY: 0,
-  },
-  enemyAmazonRight: {
-    src: "/images/amazon/right.png",
-    totalCols: 8,
-    totalRows: 1,
-    frameY: 0,
+  enemyAmazon: {
+    left: {
+      src: "/images/amazon/amazon_left.png",
+      totalCols: 8,
+      totalRows: 1,
+      frameY: 0,
+    },
+    right: {
+      src: "/images/amazon/amazon_right.png",
+      totalCols: 8,
+      totalRows: 1,
+      frameY: 0,
+    },
   },
 };
