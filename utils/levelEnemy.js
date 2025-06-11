bebabd40358e969fb9cba1048146ba27aab27046
deltaquerides <wajahat.syed@yahoo.fr>
2025-06-11 14:44:41 +0200

    add lives to enemies

--- utils/levelEnemy.js
+++ utils/levelEnemy.js
@@ -1,30 +1,30 @@
 // array of position objects for each level
 
 export const level1Enemies = [
-  { x: 40, y: 10 },
-  { x: 120, y: 10 },
-  { x: 200, y: 100 },
-  { x: 300, y: 100 },
-  { x: 600, y: 100, type: "enemyAmazon" },
-  { x: 750, y: 100 },
-  { x: 850, y: 100 },
-  { x: 970, y: 80 },
-  { x: 990, y: 80 },
-  { x: 1020, y: 80 },
-  { x: 1090, y: 80, type: "otile" },
-  { x: 1150, y: 80 },
-  { x: 1200, y: 80 },
-  { x: 1450, y: 80, type: "enemyAmazon" },
+  { x: 40, y: 10, lives: 2 },
+  { x: 120, y: 10, lives: 2 },
+  { x: 200, y: 100, lives: 2 },
+  { x: 300, y: 100, lives: 2 },
+  { x: 600, y: 100, type: "enemyAmazon", lives: 4 },
+  { x: 750, y: 100, lives: 2 },
+  { x: 850, y: 100, lives: 2 },
+  { x: 970, y: 80, lives: 2 },
+  { x: 990, y: 80, lives: 2 },
+  { x: 1020, y: 80, lives: 2 },
+  { x: 1090, y: 80, type: "otile", lives: 6 },
+  { x: 1150, y: 80, lives: 2 },
+  { x: 1200, y: 80, lives: 2 },
+  { x: 1450, y: 80, type: "enemyAmazon", lives: 4 },
 ];
 
 export const level2Enemies = [
-  { x: 100, y: 50 },
-  { x: 200, y: 100 },
-  { x: 300, y: 150 },
+  { x: 100, y: 50, lives: 2 },
+  { x: 200, y: 100, lives: 2 },
+  { x: 300, y: 150, lives: 2 },
 ];
 
 export const level3Enemies = [
-  { x: 100, y: 50 },
-  { x: 200, y: 100 },
-  { x: 300, y: 150 },
+  { x: 100, y: 50, lives: 2 },
+  { x: 200, y: 100, lives: 2 },
+  { x: 300, y: 150, lives: 2 },
 ];
