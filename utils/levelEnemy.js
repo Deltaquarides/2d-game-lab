940d3539655ab7169f639c5e8e3b0eceafcccf55
deltaquerides <wajahat.syed@yahoo.fr>
2025-06-11 14:16:56 +0200

    new enemies

--- utils/levelEnemy.js
+++ utils/levelEnemy.js
@@ -2,6 +2,7 @@
 
 export const level1Enemies = [
   { x: 40, y: 10 },
+  { x: 120, y: 10 },
   { x: 200, y: 100 },
   { x: 300, y: 100 },
   { x: 600, y: 100, type: "enemyAmazon" },
@@ -10,7 +11,7 @@ export const level1Enemies = [
   { x: 970, y: 80 },
   { x: 990, y: 80 },
   { x: 1020, y: 80 },
-  { x: 1090, y: 80 },
+  { x: 1090, y: 80, type: "otile" },
   { x: 1150, y: 80 },
   { x: 1200, y: 80 },
   { x: 1450, y: 80, type: "enemyAmazon" },
