57b438635b05f64410b2f0d7166a647768edc2a2
deltaquerides <wajahat.syed@yahoo.fr>
2025-07-07 23:48:33 +0200

    change levelData by adding map, enemies, items

--- utils/levelEnemy.js
+++ utils/levelEnemy.js
@@ -1,22 +1,5 @@
 // array of position objects for each level
 
-export const level1Enemies = [
-  { x: 40, y: 10, lives: 2 },
-  { x: 120, y: 10, lives: 2 },
-  { x: 200, y: 100, lives: 2 },
-  { x: 300, y: 100, lives: 2 },
-  { x: 600, y: 100, type: "enemyAmazon", lives: 4 },
-  { x: 750, y: 100, lives: 2 },
-  { x: 850, y: 100, lives: 2 },
-  { x: 970, y: 80, lives: 2 },
-  { x: 990, y: 80, lives: 2 },
-  { x: 1020, y: 80, lives: 2 },
-  { x: 1090, y: 80, type: "otile", lives: 10 },
-  { x: 1150, y: 80, lives: 2 },
-  { x: 1200, y: 80, lives: 2 },
-  { x: 1450, y: 80, type: "enemyAmazon", lives: 8 },
-];
-
 export const levelData = {
   1: {
     map: {
@@ -35,12 +18,34 @@ export const levelData = {
     enemies: [
       { x: 40, y: 10, lives: 2 },
       { x: 120, y: 10, lives: 2 },
-      // ... other enemies ...
+      { x: 200, y: 100, lives: 2 },
+      { x: 300, y: 100, lives: 2 },
+      { x: 600, y: 100, type: "enemyAmazon", lives: 4 },
+      { x: 750, y: 100, lives: 2 },
+      { x: 850, y: 100, lives: 2 },
+      { x: 970, y: 80, lives: 2 },
+      { x: 990, y: 80, lives: 2 },
+      { x: 1020, y: 80, lives: 2 },
+      { x: 1090, y: 80, type: "otile", lives: 10 },
+      { x: 1150, y: 80, lives: 2 },
+      { x: 1200, y: 80, lives: 2 },
+      { x: 1450, y: 80, type: "enemyAmazon", lives: 8 },
     ],
     items: [
       { x: 25, y: 55 },
       { x: 45, y: 55 },
-      // ... other items ...
+      { x: 65, y: 55 },
+      { x: 100, y: 8 },
+      { x: 120, y: 8 },
+      { x: 140, y: 8 },
+      { x: 160, y: 8 },
+      { x: 130, y: 100 },
+      { x: 150, y: 100 },
+      { x: 170, y: 100 },
+      { x: 350, y: 120 },
+      { x: 370, y: 120 },
+      { x: 390, y: 120 },
+      { x: 410, y: 120 },
     ],
   },
   2: {
