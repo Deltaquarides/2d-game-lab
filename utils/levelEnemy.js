ead4c274152be7a5e8554340d7470bbee609580e
deltaquerides <wajahat.syed@yahoo.fr>
2025-07-07 10:44:19 +0200

    levels for map, enemies,items data modifications

--- utils/levelEnemy.js
+++ utils/levelEnemy.js
@@ -1,7 +1,7 @@
 // array of position objects for each level
 
 export const level1Enemies = [
-  // { x: 40, y: 10, lives: 2 },
+  { x: 40, y: 10, lives: 2 },
   { x: 120, y: 10, lives: 2 },
   { x: 200, y: 100, lives: 2 },
   { x: 300, y: 100, lives: 2 },
@@ -17,31 +17,53 @@ export const level1Enemies = [
   { x: 1450, y: 80, type: "enemyAmazon", lives: 8 },
 ];
 
-export const level1items = [
-  { x: 25, y: 55 },
-  { x: 45, y: 55 },
-  { x: 65, y: 55 },
-  { x: 100, y: 8 },
-  { x: 120, y: 8 },
-  { x: 140, y: 8 },
-  { x: 160, y: 8 },
-  { x: 130, y: 100 },
-  { x: 150, y: 100 },
-  { x: 170, y: 100 },
-  { x: 350, y: 120 },
-  { x: 370, y: 120 },
-  { x: 390, y: 120 },
-  { x: 410, y: 120 },
-];
-
-export const level2Enemies = [
-  { x: 100, y: 50, lives: 2 },
-  { x: 200, y: 100, lives: 2 },
-  { x: 300, y: 150, lives: 2 },
-];
-
-export const level3Enemies = [
-  { x: 100, y: 50, lives: 2 },
-  { x: 200, y: 100, lives: 2 },
-  { x: 300, y: 150, lives: 2 },
-];
+export const levelData = {
+  1: {
+    map: {
+      layers: [
+        "./map/shy_Hills/l_New_Layer_1.json",
+        "./map/shy_Hills/l_New_Layer_2.json",
+        "./map/shy_Hills/l_New_Layer_3.json",
+        "./map/shy_Hills/l_New_Layer_4.json",
+        "./map/shy_Hills/l_New_Layer_5.json",
+        "./map/shy_Hills/l_New_Layer_6.json",
+        "./map/shy_Hills/l_New_Layer_7.json",
+      ],
+      collisions: "./map/shy_Hills/collisions.json",
+      tilesets: ["./images/decorations.png", "./images/tileset.png"],
+    },
+    enemies: [
+      { x: 40, y: 10, lives: 2 },
+      { x: 120, y: 10, lives: 2 },
+      // ... other enemies ...
+    ],
+    items: [
+      { x: 25, y: 55 },
+      { x: 45, y: 55 },
+      // ... other items ...
+    ],
+  },
+  2: {
+    map: {
+      layers: [
+        "./map/Dark_Cave/l_New_Layer_1.json",
+        "./map/Dark_Cave/l_New_Layer_2.json",
+        "./map/Dark_Cave/l_New_Layer_3.json",
+        "./map/Dark_Cave/l_New_Layer_4.json",
+        "./map/Dark_Cave/l_New_Layer_5.json",
+        "./map/Dark_Cave/l_New_Layer_6.json",
+        "./map/Dark_Cave/l_New_Layer_7.json",
+      ],
+      collisions: "./map/Dark_Cave/collisions.json",
+      tilesets: ["./images/decorations.png", "./images/tileset.png"],
+    },
+    enemies: [
+      { x: 100, y: 50, lives: 2 },
+      { x: 200, y: 100, lives: 2 },
+    ],
+    items: [
+      { x: 50, y: 50 },
+      { x: 70, y: 70 },
+    ],
+  },
+};
